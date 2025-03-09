7fc89ede7c776f6e33ee60dcab73e7474a4dff18
Viktor Kleen <viktor@kleen.org>
2025-03-09 15:42:57 +0000

    Fix bytes encoding and add roundtrip tests

--- syntax/Data/Syntax/Bytes/LittleEndian.hs
+++ syntax/Data/Syntax/Bytes/LittleEndian.hs
@@ -27,7 +27,7 @@ import Control.Lens ( Iso', iso, (^.), view, from )
 import qualified Data.Syntax as Syntax
 
 coerceBits :: (FiniteBits a, FiniteBits b) => a -> b
-coerceBits a = foldr (.|.) b [ bit i | i <- [0, bitSize - 1], testBit a i ]
+coerceBits a = foldr (.|.) b [ bit i | i <- [0 .. bitSize - 1], testBit a i ]
     where
         b = zeroBits
         bitSize = min (finiteBitSize a) (finiteBitSize b)
---
+++ syntax/test/BigEndianSpec.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+module BigEndianSpec where
+
+import Test.Hspec
+import Test.Hspec.QuickCheck
+
+import Data.Syntax.Bytes.BigEndian
+import Data.Word
+
+spec :: Spec
+spec = do
+  prop "coerces Word64" $ \(x :: Word64) -> do
+    x `shouldBe` coerceBits x
+
+  it "encodes big endian" $
+    fromBytes @Word32 [0x12, 0x34, 0x56, 0x78] `shouldBe` 0x12345678
+
+  it "knows the size of Word8" $ byteSize @Word8 `shouldBe` 1
+  prop "roundtrips Word8" $ \(x :: Word8) -> do
+    x `shouldBe` (fromBytes . toBytes) x
+
+  it "knows the size of Word16" $ byteSize @Word16 `shouldBe` 2
+  prop "roundtrips Word16" $ \(x :: Word16) -> do
+    x `shouldBe` (fromBytes . toBytes) x
+
+  it "knows the size of Word32" $ byteSize @Word32 `shouldBe` 4
+  prop "roundtrips Word32" $ \(x :: Word32) -> do
+    x `shouldBe` (fromBytes . toBytes) x
+
+  it "knows the size of Word64" $ byteSize @Word64 `shouldBe` 8
+  prop "roundtrips Word64" $ \(x :: Word64) -> do
+    x `shouldBe` (fromBytes . toBytes) x
