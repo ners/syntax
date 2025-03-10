24b50f791dbe32ecbf4397b3beb014ddd29806a8
ners <ners@gmx.ch>
2025-03-10 22:16:52 +0100

    add SyntaxByte

--- syntax/Data/Syntax/Bytes.hs
+++ syntax/Data/Syntax/Bytes.hs
@@ -1,13 +1,22 @@
+{-# LANGUAGE ConstraintKinds #-}
+
 module Data.Syntax.Bytes where
 
 import qualified Data.Syntax.Bytes.LittleEndian as LittleEndian
 import qualified Data.Syntax.Bytes.BigEndian as BigEndian
 import Data.Syntax
 import Data.MonoTraversable (Element)
-import Data.Word
+import Data.Word (Word8)
+import Data.ByteString (ByteString)
 
-littleEndian :: forall a syn x y. (Syntax syn, Element (Seq syn) ~ Word8, LittleEndian.Bytes a) => (LittleEndian.ByteWise a syn) x y -> syn x y
+-- | A useful synonym for Syntax with Word8 sequences.
+type SyntaxByte syn = (Syntax syn, Element (Seq syn) ~ Word8)
+
+-- | A useful synonym for Syntax with ByteString sequences.
+type SyntaxByteString syn = (Syntax syn, Seq syn ~ ByteString)
+
+littleEndian :: forall a syn x y. (SyntaxByte syn, LittleEndian.Bytes a) => (LittleEndian.ByteWise a syn) x y -> syn x y
 littleEndian (LittleEndian.ByteWise s) = s
 
-bigEndian :: forall a syn x y. (Syntax syn, Element (Seq syn) ~ Word8, BigEndian.Bytes a) => (BigEndian.ByteWise a syn) x y -> syn x y
+bigEndian :: forall a syn x y. (SyntaxByte syn, BigEndian.Bytes a) => (BigEndian.ByteWise a syn) x y -> syn x y
 bigEndian (BigEndian.ByteWise s) = s
